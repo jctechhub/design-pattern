b43a1981050b82282685c67732633667139aafcc
Jerry Chen <jcdsta@gmail.com>
2018-09-02 10:05:41 +1000

    initial

---
+++ src/src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace src
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
