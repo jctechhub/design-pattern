c9d51a4f046be3853949dbf47f54f4abde5bd332
Jerry Chen <jcdsta@gmail.com>
2018-09-19 09:04:27 +1000

    Section 3 - Lec 20 Factory method

---
+++ src/3-factory/main/main/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine(PointFactory.NewCartesianPoint(2, 3));
+            Console.WriteLine(PointFactory.NewPolarPoint(4, 5));
+            Console.Read();
+        }
+    }
+
+    public enum CoordinateSystem
+    {
+        Cartesian,
+        Polar
+    }
+
+    /// <summary>
+    /// Separate component that initialises the class in a particular way.
+    /// </summary>
+    public static class PointFactory
+    {
+        public static Point NewCartesianPoint(double x, double y)
+        {
+            return new Point(x, y);
+        }
+
+        public static Point NewPolarPoint(double rho, double theta)
+        {
+            return new Point(rho*Math.Cos(theta), rho*Math.Sin(theta));
+        }
+    }
+
+    public class Point
+    {
+        private double x, y;
+
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(x)}: {x}, {nameof(y)}: {y}";
+        }
+    }
+}
