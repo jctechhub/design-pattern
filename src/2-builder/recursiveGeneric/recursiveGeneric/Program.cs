863e5fe0d82aa1def1cfe33fcf516194815019e2
Jerry Chen <jcdsta@gmail.com>
2019-03-16 19:58:19 +1100

    update facade builder

--- src/2-builder/recursiveGeneric/recursiveGeneric/Program.cs
+++ src/2-builder/recursiveGeneric/recursiveGeneric/Program.cs
@@ -2,54 +2,102 @@
 
 namespace recursiveGeneric
 {
-
-
-
-
-
-    public class Person
-    {
-        public string Name;
-
-        public string Position;
-
-        //     class Builder : PersonInfoBuilder<Builder> { /* degenerate */ }
-        public override string ToString()
-        {
-            return $"{nameof(Name)}: {Name}, {nameof(Position)}: {Position}";
-        }
-    }
-
-    public class PersonInfoBuilder
-    {
-        protected Person person = new Person();
-
-        public PersonInfoBuilder Called(string name)
-        {
-            person.Name = name;
-            return this;
-        }
-    }
-
-
-    public class PersonJobBuilder : PersonInfoBuilder
-    {
-        public PersonJobBuilder WorkAsA(string position)
-        {
-            person.Position = position;
-            return this;
-        }
-    }
-
-
-
     class Program
     {
         static void Main(string[] args)
         {
-            var builder = new PersonJobBuilder();
-            builder.Called("Tom")
-                .WorkAsA  //can't do this, compile error. 
+            var pb = new PersonBuilder();
+            Person person = pb.Works
+                                .At("Microsoft")
+                                .AsA("PM")
+                                .Earning(150000)
+                            .Lives
+                                .At("101 pitt street")
+                                .In("Sydney")
+                                .WithPostcode("2000");
+
+
+            Console.WriteLine(person);
         }
     }
+
+
+    public class Person
+    {
+        public string StreetAddress, Postcode, City;
+        public string CompanyName, Position;
+        public int AnnualIncome;
+
+        public override string ToString()
+        {
+            return $"{nameof(StreetAddress)}: {StreetAddress}, {nameof(Postcode)}: {Postcode}, {nameof(City)}: {City}, {nameof(CompanyName)}: {CompanyName}, {nameof(Position)}: {Position}, {nameof(AnnualIncome)}: {AnnualIncome}";
+        }
+
+    }
+
+
+    public class PersonBuilder //facade builder
+    {
+        protected Person person = new Person();
+
+        public PersonJobBuilder Works => new PersonJobBuilder(person);
+        public PersonAddressBuilder Lives => new PersonAddressBuilder(person);
+        public static implicit operator Person(PersonBuilder pb) => pb.person;
+
+    }
+
+    public class PersonJobBuilder : PersonBuilder
+    {
+        public PersonJobBuilder(Person person)
+        {
+            //1. pass to the base class
+            this.person = person;
+        }
+        public PersonJobBuilder At(string company)
+        {
+            person.CompanyName = company;
+            return this;
+        }
+
+        public PersonJobBuilder AsA(string position)
+        {
+            person.Position = position;
+            return this;
+        }
+
+        public PersonJobBuilder Earning(int income)
+        {
+            person.AnnualIncome = income;
+            return this;
+        }
+    }
+
+    public class PersonAddressBuilder : PersonBuilder
+    {
+        // might not work with a value type!
+        public PersonAddressBuilder(Person person)
+        {
+            this.person = person;
+        }
+
+        public PersonAddressBuilder At(string streetAddress)
+        {
+            person.StreetAddress = streetAddress;
+            return this;
+        }
+
+        public PersonAddressBuilder WithPostcode(string postcode)
+        {
+            person.Postcode = postcode;
+            return this;
+        }
+
+        public PersonAddressBuilder In(string city)
+        {
+            person.City = city;
+            return this;
+        }
+
+    }
+
 }
